9d20cce9e763ebacf3827c52a4dd56f0de1d249b
lenon777 <amytsak90@gmail.com>
2019-08-19 21:26:24 +0300

    delete coments

--- src/code.ts
+++ src/code.ts
@@ -124,58 +124,6 @@ function sequenceWithTimeAndContinue(amount: number): void {
     })
 }
 
-// function sequenceWithTimeAndContinue(amount: number): void {
-//     let i = 1;
-//     nextFetch(0);
-//     function nextFetch(delay: number) {
-//         let rez = '';
-//         let data$ = fromFetch(`https://jsonplaceholder.typicode.com/users/${i}`).pipe(
-//             switchMap(response => {
-//                 if (response.ok && delay <= 4000) {
-
-//                     return response.json();
-//                 } else {
-//                     return of({ error: true, message: `Error ${response.status}` });
-//                 }
-//             }),
-//             catchError(err => {
-//                 console.error(err);
-//                 return of({ error: true, message: err.message })
-//             })
-//         );
-//         i++;
-//         data$.subscribe({
-//             next: result => {
-//                 console.log(result)
-//                 rez = result.name;
-//             },
-//             complete: () => {
-//                 const delay = Math.random() * (5000 - 1000) + 1000;
-//                 const ul = document.getElementById('output');
-//                 const li = document.createElement("li");
-//                 if (i <= amount) {
-//                     if (rez !== undefined) {
-//                         setTimeout(() => { nextFetch(delay) }, delay)
-//                     } else {
-//                         rez = "Too much time, Press the button if you want to continue fetching"
-
-//                         li.classList.add("warning");
-//                         const btn = document.createElement("button");
-//                         btn.innerText = "Continue";
-//                         document.getElementsByTagName('body')[0].append(btn);
-//                         btn.addEventListener('click', () => {
-//                             nextFetch(0),
-//                                 btn.remove();
-//                         })
-
-//                     }
-//                     li.innerText = rez;
-//                     ul.append(li);
-//                 }
-//             }
-//         })
-//     }
-// }
 document.getElementById('sequenceWithTimeAndContinue').addEventListener('click', () => {
     clearList();
     sequenceWithTimeAndContinue(10)
